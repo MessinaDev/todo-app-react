c8f69587f6ef06a137e5776b863a7498e44d1b91
Daniele Messina <messina.dev@gmail.com>
2024-03-17 22:21:03 +0100

    Add button to change theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -6,12 +6,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import NewTodo from "./components/NewTodo";
 import TaskList from "./components/TaskList";
 
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
 import { useSelector } from "react-redux";
 
 export default function App() {
-  const darkTheme = createTheme({
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  const theme = createTheme({
     palette: {
-      mode: "dark",
+      mode: darkTheme ? "dark" : "light",
     },
   });
 
@@ -20,12 +26,18 @@ export default function App() {
   });
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
         <header>
           <h1>My tasks</h1>
         </header>
+        <IconButton
+          className="ButtonTheme"
+          onClick={() => setDarkTheme((prev) => !prev)}
+        >
+          {darkTheme ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
         <NewTodo />
         <TaskList tasks={tasks} />
       </div>
