a46163a96813ebe1abb7b1027ec49d3c1a628bee
Daniele Messina <messina.dev@gmail.com>
2024-04-14 19:04:51 +0200

    added redux logger and middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import taskListSlice from "store/reducers/taskListSlice";
 import alertSlice from "store/reducers/alertSlice";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     taskList: taskListSlice,
     alert: alertSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 export default store;
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
